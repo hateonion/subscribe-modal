3b3dbb81aba0fdff52d75a5fc71be83314dfd146
hateonion <ouyang_yingjie@outlook.com>
2017-11-19 23:04:12 +0800

    finish basic style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import * as Cookies from 'js-cookie';
+import 'font-awesome/css/font-awesome.min.css';
+import _ from 'lodash';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+$(document).ready(() => {
+  $('#test-button').on('click', (e) => {
+    e.preventDefault();
+  })
+})
