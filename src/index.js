64e635956c652f28dbd0b3cd8f94fd5d5a4f98f9
hateonion <ouyang_yingjie@outlook.com>
2017-11-20 15:44:05 +0800

    Add cookie check

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import _ from 'lodash';
 import './index.css';
 
 $(document).ready(() => {
-  $('#test-button').on('click', (e) => {
-    e.preventDefault();
-    $('#subscribe-modal').css('display', 'flex');
-  });
+  if(Cookies.get('IsNewsLetterSubscribed')) {
+    return;
+  }
+  $('#subscribe-modal').css('display', 'flex');
   $('#subscribe-modal-close').on('click', (e) => {
     e.preventDefault();
     $('#subscribe-modal').css('display', 'none');
   });
+  $('#subscribe-modal-submit').on('click', () => {
+    Cookies.set('IsNewsLetterSubscribed', true);
+  })
 })
