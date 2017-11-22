df98decc0f677e08420601ccee0e66f77febbd51
hateonion <ouyang_yingjie@outlook.com>
2017-11-22 21:57:28 +0800

    add animation

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import * as Cookies from 'js-cookie';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 
+const popupModal = () => {
+    $('#subscribe-modal').css('transform', 'scale(1)');
+}
+
 $(document).ready(() => {
   if(Cookies.get('IsNewsLetterSubscribed') || Cookies.get('IsUserManualClose')) {
     return;
   }
-  $('#subscribe-modal').css('display', 'flex');
+    window.setTimeout(popupModal, 500);
   $('#subscribe-modal-close').on('click', (e) => {
     e.preventDefault();
     Cookies.set('IsUserManualClose', true, { expires: 1});
-    $('#subscribe-modal').css('display', 'none');
+      $('#subscribe-modal').css('transform', 'scale(0)');
   });
   $('#subscribe-modal-submit').on('click', () => {
     Cookies.set('IsNewsLetterSubscribed', true);
